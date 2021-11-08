1683a48beca06f224d897ee3c191c9fb4fed92a7
eharn <77076617+eharn@users.noreply.github.com>
2021-11-08 13:36:31 -0700

    team week 9

--- assignment9/js/activity.js
+++ assignment9/js/activity.js
@@ -1,6 +1,47 @@
-for (let item of document.querySelectorAll(".keys div")) {
-    item.addEventListener("click", function (e){
-        e.target.classList.add("active");
-    }, false);
-    console.log(item);
+var key = document.querySelectorAll(".key");
+
+Array.from(key)
+  .forEach(btn => btn.addEventListener('click', () => getSound()));
+
+var soundFile = document.querySelectorAll("audio");
+
+
+var audioDuration;
+var sound;
+
+
+function getSound(){
+  //get the data-key from the <div> that got clicked
+
+var keySound = window.event.target.parentElement.attributes["data-key"].value
+  console.log(keySound);
+var playingBtn = window.event.target.parentElement;
+  //add that class name of 'playing' to the existing class list
+
+soundFile.forEach(audioElement => {
+var soundbyte = audioElement.getAttribute("data-key");
+console.log(soundbyte);
+
+    //match audio data-key to div data-key
+    if(soundbyte === keySound){
+      playSound(audioElement);
+      playingBtn.classList.add('playing');
+
+    }
+  //gets the length of the audio file and makes it longer by multiplying
+  var audioDuration = audioElement.duration * 2000;
+
+    //function to timeout and remove the playing class, takes a generic function
+    setTimeout(function() {
+    playingBtn.classList.remove('playing');
+    }, audioDuration);
+    })
+
+   //if a single ket gets pressed 10 times, then remove the .moveButton class
+   //count how many 
+}
+ function playSound(x) {
+   //#2 of core requirements, make sound play faster
+  x.currentTime = 0;
+  x.play()
 }
